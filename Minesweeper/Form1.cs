dc75bc4fcb1a3820a7fe16d4c3f30bd0791cba78
lef <you@example.com>
2022-08-29 22:14:25 +0200

    Ну начнем с сеточки

---
+++ Minesweeper/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        MinesweeperGame minesweeperGame = new MinesweeperGame(9, 9, 30);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            minesweeperGame.Draw(e.Graphics);
+        }
+    }
+}
---
+++ Minesweeper/MinesweeperGame.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Minesweeper
+{
+    class MinesweeperGame
+    {
+        private readonly int gameFieldHeightInCells;
+        private readonly int gameFieldWidthInCells;
+        private readonly int cellSize;
+
+        public MinesweeperGame(int gameFieldHeightInCells, int gameFieldWidthInCells, int cellSize)
+        {
+            this.gameFieldHeightInCells = gameFieldHeightInCells;
+            this.gameFieldWidthInCells = gameFieldWidthInCells;
+            this.cellSize = cellSize;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            for (int y = 0; y <= gameFieldHeightInCells; y++)
+            {
+                graphics.DrawLine(Pens.Black, 0, y * cellSize, gameFieldWidthInCells * cellSize, y * cellSize);
+            }
+
+            for (int x = 0; x <= gameFieldWidthInCells; x++)
+            {
+                graphics.DrawLine(Pens.Black, x * cellSize, 0, x * cellSize, gameFieldHeightInCells * cellSize);
+            }
+        }
+    }
+}
